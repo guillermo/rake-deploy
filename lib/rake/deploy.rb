c8e409b0b8b65799caa112712c0936d53f9cc717
Guillermo Álvarez <guillermo@cientifico.net>
2010-06-22 18:23:20 +0200

    Remove the explicity host when calling deploy.run(deploy.host)

--- lib/rake/deploy.rb
+++ lib/rake/deploy.rb
@@ -17,38 +17,38 @@ namespace :deploy do
   desc 'Update remote repo'
   task :update_repo do
     puts "=> update_repo"
-    deploy.run(deploy.host, "cd #{deploy.deploy_to}/shared/repo && git fetch")
+    deploy.run("cd #{deploy.deploy_to}/shared/repo && git fetch")
   end
   
   desc 'Checkout newer version'
   task :checkout => [:generate_timestamp,:update_repo] do
     puts "=> checkout"
-    deploy.run(deploy.host, "mkdir -p #{deploy.deploy_to}/releases/")
-    deploy.run(deploy.host, "git clone -s #{deploy.deploy_to}/shared/repo #{deploy.release_path} && cd #{deploy.release_path} && git checkout #{deploy.branch}")
+    deploy.run("mkdir -p #{deploy.deploy_to}/releases/")
+    deploy.run("git clone -s #{deploy.deploy_to}/shared/repo #{deploy.release_path} && cd #{deploy.release_path} && git checkout #{deploy.branch}")
     deploy.shared.each do |shared_path|
-      deploy.run(deploy.host, "rm -rf #{deploy.release_path}#{shared_path}")
-      deploy.run(deploy.host, "ln -s #{deploy.deploy_to}/shared/scaffold#{shared_path} #{deploy.release_path}#{shared_path}")
+      deploy.run("rm -rf #{deploy.release_path}#{shared_path}")
+      deploy.run("ln -s #{deploy.deploy_to}/shared/scaffold#{shared_path} #{deploy.release_path}#{shared_path}")
     end
   end
   
   desc 'Symlink to new version'
   task :symlink => [:checkout] do
     puts "=> symlink"
-    deploy.run(deploy.host, "unlink #{deploy.deploy_to}/current")
-    deploy.run(deploy.host, "ln -s #{deploy.release_path} #{deploy.deploy_to}/current")
+    deploy.run("unlink #{deploy.deploy_to}/current")
+    deploy.run("ln -s #{deploy.release_path} #{deploy.deploy_to}/current")
   end  
 
   desc 'Setup' 
   task :setup do
     puts "=> setup"
     %w(/releases /shared/scaffold/tmp /shared/scaffold/log).each do |path|
-      deploy.run(deploy.host, "mkdir -p #{deploy.deploy_to}#{path}")
+      deploy.run("mkdir -p #{deploy.deploy_to}#{path}")
     end
     
-    deploy.run(deploy.host, "ln -s #{deploy.deploy_to}/shared/scaffold/tmp #{deploy.deploy_to}/shared/tmp ")
-    deploy.run(deploy.host, "ln -s #{deploy.deploy_to}/shared/scaffold/log #{deploy.deploy_to}/shared/log ")
+    deploy.run("ln -s #{deploy.deploy_to}/shared/scaffold/tmp #{deploy.deploy_to}/shared/tmp ")
+    deploy.run("ln -s #{deploy.deploy_to}/shared/scaffold/log #{deploy.deploy_to}/shared/log ")
     
-    deploy.run(deploy.host, "git clone --bare #{deploy.repository} #{deploy.deploy_to}/shared/repo")
+    deploy.run("git clone --bare #{deploy.repository} #{deploy.deploy_to}/shared/repo")
   end
   
 end
